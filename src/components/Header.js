b8367d345d3118bee175cb2c9d5017c05705416e
최동빈 <dbchoi0512@naver.com>
2023-06-27 18:03:21 +0900

    header page little

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
-    return <div className="header">header</div>;
+    return (
+        <div className="header">
+            <div className="header-red" />
+            <div className="header-image">
+                <div className="line">
+                    <img
+                        className="pupil-dilation"
+                        alt="my-header"
+                        src="img/header_img.png"
+                    />
+                    <input type="text" id="search" name="search" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
