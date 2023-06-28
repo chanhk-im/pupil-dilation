e0d15444d82820fecb5707f416fc34c75b28df83
chanhk-im <cfasd1875@gmail.com>
2023-06-28 16:03:04 +0900

    feat[#7]: redux store 구성

---
+++ src/stores/store.js
@@ -0,0 +1,23 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
+import persistStore from 'redux-persist/es/persistStore';
+import showReducer from '../features/slices/showSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const reducers = combineReducers({
+    show: showReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;
