89ecbfc8ab2fa83e9aff94d83df3cfacb3781c1d
chanhk-im <cfasd1875@gmail.com>
2023-06-28 11:53:40 +0900

    feat[#4]: header 보완

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,35 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
-            <div className="header-red" />
-            <div className="header-image">
-                <div className="line">
+            <div className="header-red">
+                <div className="auth">로그인</div>
+                <div className="auth">회원가입</div>
+            </div>
+            <div className="line">
+                <img
+                    className="pupil-dilation"
+                    alt="my-header"
+                    src="img/header_img.png"
+                />
+                <div className="line-right">
+                    <div className="search-input">
+                        <input
+                            type="text"
+                            id="search"
+                            name="search"
+                            placeholder="공연/동아리 검색"
+                        />
+                        <img
+                            className="search-button"
+                            alt="search-button"
+                            src="images/search.png"
+                        />
+                    </div>
                     <img
-                        className="pupil-dilation"
-                        alt="my-header"
-                        src="img/header_img.png"
+                        className="user-button"
+                        alt="search-button"
+                        src="images/user.svg"
                     />
-                    <input type="text" id="search" name="search" />
                 </div>
             </div>
         </div>
