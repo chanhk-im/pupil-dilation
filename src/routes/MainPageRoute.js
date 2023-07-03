2b4277d82541f4d9d4b1f2a36597f3f8e641d3a1
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2023-07-03 16:55:41 +0900

    feat[#32]: 주최자 공연관리 페이지 추가

--- src/routes/MainPageRoute.js
+++ src/routes/MainPageRoute.js
@@ -7,11 +7,8 @@ import SignUpPage from '../components/SignUpPage';
 import Seats from '../components/Seats/Seats';
 import TicketList from '../components/TicketList';
 import HostMainPage from '../components/HostMainPage';
-<<<<<<< HEAD
 import HostEventManage from '../components/HostEventManage';
-=======
 import Loading from '../components/Loading';
->>>>>>> dev
 
 function MainPageRoute() {
     return (
@@ -24,14 +21,11 @@ function MainPageRoute() {
                 <Route path="/seats" element={<Seats />} />
                 <Route path="/ticketlist" element={<TicketList />} />
                 <Route path="/HostMain" element={<HostMainPage />} />
-<<<<<<< HEAD
                 <Route
                     path="/HostEventManage/:id"
                     element={<HostEventManage />}
                 />
-=======
                 <Route path="/loading" element={<Loading />} />
->>>>>>> dev
             </Routes>
         </div>
     );
