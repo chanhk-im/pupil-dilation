958be71c2ae12c749bd339be77e852f22c0ffea9
chanhk-im <cfasd1875@gmail.com>
2023-07-05 11:28:09 +0900

    feat[#60]: 메인 페이지 db와 연결

--- src/components/Main/desktop/SliderDesktop.js
+++ src/components/Main/desktop/SliderDesktop.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'swiper/swiper-bundle.min.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { Navigation, Pagination, A11y, Zoom, Autoplay } from 'swiper';
 import './SliderDesktop.css';
 
 // eslint-disable-next-line no-shadow
-function SliderDesktop({ slides }) {
+function SliderDesktop() {
+    const showList = useSelector((state) => state.show.showList);
+
     return (
         <Swiper
             modules={[Navigation, Pagination, A11y, Zoom, Autoplay]}
@@ -31,10 +33,10 @@ function SliderDesktop({ slides }) {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
         >
-            {slides.map((slide) => (
+            {showList.map((slide) => (
                 <SwiperSlide className="eventBoard" key={slide.image}>
                     <Link
-                        to="/detail/BkmL00lDIRKbcwMLPqDc"
+                        to={`/detail/${slide.id}`}
                         style={{ textDecoration: 'none' }}
                     >
                         <div className="poster">
@@ -46,20 +48,17 @@ function SliderDesktop({ slides }) {
                             />
                             <img
                                 className="eventImage"
-                                src={slide.image}
+                                src="images/Dongari1.png"
                                 alt={slide.title}
                             />
                         </div>
                         <p className="eventName">{slide.title}</p>
-                        <p className="eventDate">{slide.date}</p>
+                        <p className="eventDate">{slide.period}</p>
                     </Link>
                 </SwiperSlide>
             ))}
         </Swiper>
     );
 }
-SliderDesktop.propTypes = {
-    slides: PropTypes.node.isRequired,
-};
 
 export default SliderDesktop;
