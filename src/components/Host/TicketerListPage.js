1a7e21b8f74fb9b131f631d7ce5c65ff97f9bdbb
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2023-07-10 14:53:09 +0900

    feat[#85]:체크박스 상태변화 텍스트 색변경

--- src/components/Host/TicketerListPage.js
+++ src/components/Host/TicketerListPage.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/TicketerListPage.css';
 
-
-
+/*eslint-disable*/
 function TicketerListPage() {
+    const [totalRowCnt, setTotalRowCnt] = useState(0);
+    const [toggleState, setToggleState] = useState(false);
+
+    function toggleStateChange() {
+        setToggleState(!toggleState);
+    }
+
+    useEffect(() => {
+        const table = document.getElementById('mytable');
+        const totalRowCnt = table?.rows?.length || 0;
+        setTotalRowCnt(totalRowCnt);
+    }, []);
+
     return (
         <>
             <div className="ticketer-header">
                 <h1 className="title-main">예매자 목록</h1>
-                <h1 className="title-sub">총 1명</h1>
+                <h1 className="title-sub">총 {totalRowCnt - 1}명</h1>
             </div>
             <div className="ticketer-list-container">
-                <table className="tablet">
+                <table className="tablet" id="mytable">
                     <thead>
                         <tr>
                             <th>목록</th>
@@ -32,14 +44,26 @@ function TicketerListPage() {
                                 <section className="model-1">
                                     <p>입금 전</p>
                                     <div className="checkbox">
-                                        <input type="checkbox" id="check" />
+                                        <input
+                                            type="checkbox"
+                                            id="check"
+                                            onClick={toggleStateChange}
+                                        />
                                         <label htmlFor="check" />
                                     </div>
-                                    <p>입금 후</p>
+                                    <p>입금 완료</p>
                                 </section>
                             </td>
                             <td>
-                                <p>예매중</p>
+                                <p
+                                    style={
+                                        toggleState
+                                            ? { color: 'blue' }
+                                            : { color: 'red' }
+                                    }
+                                >
+                                    {toggleState ? '예매 완료' : '예매 중'}
+                                </p>
                             </td>
                         </tr>
                     </tbody>
