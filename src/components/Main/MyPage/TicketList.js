7a104946846c6266d1d900591a7a7024ad558dee
limenow <ime2me12@naver.com>
2023-07-26 18:02:06 +0900

    feat[#123]:마이페이지 구현

--- src/components/Main/MyPage/TicketList.js
+++ src/components/Main/MyPage/TicketList.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicketList.css';
 
 function TicketList() {
+    const [ticketDown, setTicketDown] = useState(false);
     return (
         <div className="user-mypage-container">
             <div className="user-mypage-text">My Page</div>
             <div className="ticket-list">
-                <div className="ticket-reservation">예매내역</div>
+                <div className="ticket-reservation">최근 예매내역</div>
                 <div className="opened-ticket">
                     <div className="opened-ticket-high">
-                        <img
-                            className="arrow-down"
-                            alt="arrow-down"
-                            src="/images/arrow-right.svg"
-                        />
+                        <button
+                            className="arrow"
+                            onClick={() => {
+                                setTicketDown((ticketDown) => !ticketDown);
+                            }}
+                        >
+                            {ticketDown ? (
+                                <img
+                                    src="/images/arrow-right.svg"
+                                    alt="arrow-right"
+                                />
+                            ) : (
+                                <img
+                                    src="/images/arrow-down.svg"
+                                    alt="arrow-down"
+                                />
+                            )}
+                        </button>
                         <div className="ticket-show-title">공연 제목</div>
                         <div className="ticket-show-time">
                             2000.00.00(토) 22:00
                         </div>
                         <div className="ticket-show-info">공연정보</div>
                     </div>
-                    <div className="ticket-hidden">
+                    <nav className={ticketDown ? 'ticket-hidden' : ''}>
+                        {/* <div className="ticket-hidden"> */}
                         <div className="ticket-picture">
                             <img
                                 className="dongari1"
@@ -52,27 +67,8 @@ function TicketList() {
                                 결제확인중
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="closed-ticket">
-                    <img
-                        className="arrow-right"
-                        alt="arrow-right"
-                        src="/images/arrow-right.svg"
-                    />
-                    <div className="show-title">공연 제목</div>
-                    <div className="show-time">2000.00.00(토) 22:00</div>
-                    <div className="ticket-opened-right">결제확인완료</div>
-                </div>
-                <div className="closed-ticket">
-                    <img
-                        className="arrow-right"
-                        alt="arrow-right"
-                        src="/images/arrow-right.svg"
-                    />
-                    <div className="show-title">공연 제목</div>
-                    <div className="show-time">2000.00.00(토) 22:00</div>
-                    <div className="ticket-opened-right">결제확인중</div>
+                        {/* </div> */}
+                    </nav>
                 </div>
             </div>
         </div>
