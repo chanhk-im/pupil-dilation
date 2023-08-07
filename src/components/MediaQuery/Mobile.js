31e23417aa85750b3e073bb0e9a32e8f96d9a41a
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2023-08-07 12:05:28 +0900

    feat[#202]: Detail 페이지 수정

--- src/components/MediaQuery/Mobile.js
+++ src/components/MediaQuery/Mobile.js
@@ -1,7 +1,7 @@
 import { useMediaQuery } from 'react-responsive';
 
 function Mobile({ children }) {
-    const isMobile = useMediaQuery({ maxWidth: 400 });
+    const isMobile = useMediaQuery({ maxWidth: 500 });
     return isMobile ? children : null;
 }
 export default Mobile;
