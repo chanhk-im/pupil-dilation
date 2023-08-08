5320fa7746cbd7d59c0a34832234ade6f99bc11d
eastbean <dbchoi0512@naver.com>
2023-08-08 17:24:02 +0900

    feat[#206]: make delete modal

--- src/components/Host/HostEventManage.js
+++ src/components/Host/HostEventManage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './HostEventManage.css';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -6,20 +6,43 @@ import { ref, deleteObject } from 'firebase/storage';
 import HostEventContent from './HostEventContent';
 import { fStorage } from '../../Firebase';
 import { deleteShowsDocument } from '../../features/show/api/showsDocumentApi';
+import DeletePopup from '../Popup/DeletePopup';
 
 function HostEventManage() {
     const navigate = useNavigate();
     function getIndex(showList, id) {
         return showList.findIndex((element) => element.id === id);
     }
-
+    const [popup, setPopup] = useState({
+        open: false,
+        message: '',
+        callback: false,
+    });
     const { id } = useParams();
     const showList = useSelector((state) => state.show.showList);
     const index = getIndex(showList, id);
     const imageRef = ref(fStorage, showList[index].image);
 
+    const handleDeleteButtonOnClick = () => {
+        setPopup({
+            open: true,
+            message: '정말로 삭제하시겠습니까?',
+            callback: () => {
+                deleteShowsDocument(id);
+                navigate('/host');
+                deleteObject(imageRef);
+            },
+        });
+    };
+
     return (
         <div className="detail">
+            <DeletePopup
+                open={popup.open}
+                setPopup={setPopup}
+                message={popup.message}
+                callback={popup.callback}
+            />
             <div className="buttonBoard">
                 <button
                     className="updateButton"
@@ -31,11 +54,7 @@ function HostEventManage() {
                 <button
                     className="delateButton"
                     type="button"
-                    onClick={() => {
-                        deleteShowsDocument(id);
-                        navigate('/host');
-                        deleteObject(imageRef);
-                    }}
+                    onClick={handleDeleteButtonOnClick}
                 >
                     공연 삭제
                 </button>
