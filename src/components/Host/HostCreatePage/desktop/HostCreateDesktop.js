4ec7965016f6484f99a61d0f4bc27adff925a391
chanhk-im <cfasd1875@gmial.com>
2023-09-09 20:39:29 +0900

    fix: host create 이미지 업로드 오류 수정

--- src/components/Host/HostCreatePage/desktop/HostCreateDesktop.js
+++ src/components/Host/HostCreatePage/desktop/HostCreateDesktop.js
@@ -257,7 +257,15 @@ function HostCreateDesktop() {
                     callback={popup.callback}
                 />
                 <div className="host-create-left">
-                    <input type="file" accept="image/*" required multiple />
+                    <input
+                        type="file"
+                        onChange={(event) => {
+                            setImageUpload(event.target.files[0]);
+                        }}
+                        accept="image/*"
+                        required
+                        multiple
+                    />
                 </div>
                 <div className="host-create-right">
                     <div className="host-create-right-1">
