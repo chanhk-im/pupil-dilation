6e85015ba90df4fe56b00930f3c7c7407941fb3e
chanhk-im <cfasd1875@gmail.com>
2023-09-10 23:47:02 +0900

    fix[#326]: host update css 수정

--- src/hooks/useMainPageLoading.js
+++ src/hooks/useMainPageLoading.js
@@ -35,7 +35,6 @@ export default function useMainPageLoading() {
                     console.log(element);
                 });
             });
-            console.log(showList);
         });
     }, []);
 
